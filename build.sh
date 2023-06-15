8ee306fe04043e3ee15d13fba1315439c8c1e40e
Welly_Orc <garret.besser@acc.co.nz>
2023-06-15 15:16:00 +1200

    remove unnecessary scripts

--- build.sh
+++
@@ -1,8 +0,0 @@
-
-# JAVA -jar ../publisher/publisher.jar -ig . #  -tx n/a
-
-# java -jar ../publisher/publisher.jar -ig . # -tx https://r4.ontoserver.csiro.au/fhir
-
-%JAVA_HOME%\bin\java -jar ../sushi/fhir-ig-publisher/org.hl7.fhir.publisher.cli/target/org.hl7.fhir.publisher.cli-1.2.14-SNAPSHOT.jar -ig .
-
-# https://r4.ontoserver.csiro.au/fhir
